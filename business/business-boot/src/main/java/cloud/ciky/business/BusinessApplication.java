ff822e91ad4080e0d2c74d33877bd52c3c4d35e4
ciky <564140599@qq.com>
2026-03-14 11:16:12 +0800

    fix: business服务引入system服务feign

--- business/business-boot/src/main/java/cloud/ciky/business/BusinessApplication.java
+++ business/business-boot/src/main/java/cloud/ciky/business/BusinessApplication.java
@@ -1,9 +1,11 @@
 package cloud.ciky.business;
 
+import cloud.ciky.system.api.UserFeignClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <p>
@@ -16,6 +18,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication()
 @EnableDiscoveryClient
 @MapperScan({"cloud.ciky.business.mapper"})
+@EnableFeignClients(basePackageClasses = {UserFeignClient.class})
 public class BusinessApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessApplication.class, args);
